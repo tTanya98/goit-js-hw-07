9c8ca4a0f3d0592a2e3690200791548fa18807e2
tTanya98 <chichkinat98@gmail.com>
2023-03-03 20:47:14 +0200

    Update tsk-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.addEventListener('click', event => {
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    
+console.log(event);
+});
+
+
+
+
 
 console.log(galleryItems);
