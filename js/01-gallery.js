aab34f9bd7abc509b8ee16c3852a8891ff9375f2
tTanya98 <chichkinat98@gmail.com>
2023-03-04 20:08:47 +0200

    Update tsk-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,17 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryEl = document.querySelector('.gallery');
+const galleryIt = createGalleryItemsMarkup(galleryItems);
 
-galleryEl.addEventListener('click', event => {
-    if (event.target.nodeName !== 'IMG') {
-        return;
-    }
-    
-console.log(event);
-});
+galleryEl.insertAdjacentHTML('beforeend', galleryIt);
 
+function createGalleryItemsMarkup (galleryItems) {
+return galleryItems.map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`; }).join('');
+}
 
-
+// galleryEl.addEventListener('click', event => {
+//     if (event.target.nodeName !== 'IMG') {
+//         return;
+//     }
+// });
 
 
 console.log(galleryItems);
