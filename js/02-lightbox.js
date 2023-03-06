495cc341e659bcd73a3ed6d10086bd8acc7d5d1f
tTanya98 <chichkinat98@gmail.com>
2023-03-06 15:26:14 +0200

    Update tsk-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+const galleryIt = createGalleryItemsMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryIt);
+
+function createGalleryItemsMarkup (galleryItems) {
+return galleryItems.map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" 
+    src="${preview}" 
+    alt="${description}" />
+  </a>`; }).join('');
+}
+
+const lightbox = new SimpleLightbox(`.gallery__item`, {
+    captionsData: "alt",
+    captionDelay: 250,
+});
 
 console.log(galleryItems);
